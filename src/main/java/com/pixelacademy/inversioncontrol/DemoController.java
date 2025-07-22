c09efa4e7cc2e96af09cd523264334840ebd7883
marianbt554 <botnarencomarian8@gmail.com>
2025-07-22 18:29:51 +0200

    Mapping commit

---
+++ src/main/java/com/pixelacademy/inversioncontrol/DemoController.java
@@ -0,0 +1,22 @@
+package com.pixelacademy.inversioncontrol;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Chef myChef;
+
+    @Autowired
+    public DemoController (Chef theChef) {
+        myChef = theChef;
+    }
+
+    @GetMapping("/daily-recipe")
+    public String getDailyRecipe() {
+        return  myChef.getDailyRecipe();
+
+    }
+}
