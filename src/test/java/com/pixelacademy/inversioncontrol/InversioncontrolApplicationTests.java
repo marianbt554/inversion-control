1b1437a546c2b4d627406abdbcae6822926f0ba1
marianbt554 <botnarencomarian8@gmail.com>
2025-07-22 17:46:27 +0200

    Initial commit

---
+++ src/test/java/com/pixelacademy/inversioncontrol/InversioncontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelacademy.inversioncontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InversioncontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
